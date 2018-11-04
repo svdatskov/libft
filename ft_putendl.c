360e24229d634ddbdc6c122fbc7e1c772ad4fa77
Sviatoslav Datskov <svdatskov@MacBook-Air-Sviatoslav.local>
2018-11-04 03:02:31 +0200

    new

---
+++ ft_putendl.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_putendl(char const *s)
+{
+	int i;
+
+	i = 0;
+	if (s != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			ft_putchar(s[i]);
+			i++;
+		}
+		ft_putchar('\n');
+	}
+}
---
+++ ft_putnbr.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+
+void	ft_print(int i, int n)
+{
+	int j;
+
+	while (i >= 1)
+	{
+		j = n / i;
+		ft_putchar(j + 48);
+		n %= i;
+		i /= 10;
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	int i;
+	int num;
+
+	num = n;
+	i = 1;
+	while (num > 9 || num < -9)
+	{
+		i *= 10;
+		num /= 10;
+	}
+	if(n < 0)
+	{
+		ft_putchar('-');
+		if (n < -2147483647)
+		{
+			ft_putchar('2');
+			n = -147483648;
+			i /= 10;
+		}
+		n = -n;
+	}
+	ft_print(i, n);
+}
